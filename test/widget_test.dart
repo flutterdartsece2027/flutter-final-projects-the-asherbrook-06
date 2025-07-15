a1da30a175098cae5e49fc6d59b056cba227f8a0
Santhosh Sivakumar <2005.santhoshsivakumar@gmail.com>
2025-07-15 19:15:00 +0530

    Modified: Updated AppWidgetName

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:buzz/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Linker());
+    await tester.pumpWidget(const Buzz());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
